5003d20c7c1f9cbf379a533a6a3a4356d86f06c1
Maxime Zarka <maxime.zarka@gmail.com>
2018-11-26 18:20:02 +0100

    finished booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+
   def new
     @idea = Idea.find(params[:idea_id])
     @booking = Booking.new(idea: @idea)
@@ -8,28 +9,31 @@ class BookingsController < ApplicationController
     @idea = Idea.find(params[:idea_id])
     @booking = Booking.new(booking_params)
     @booking.idea = @idea
+    @booking.user = current_user
     if @booking.save
-      redirect_to idea_path(@idea)
+      render :show
     else
       render :new
     end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
-  end
-
-  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :request_message)
+    params.require(:booking).permit(:start_date, :end_date, :request_message, :validated)
   end
 end
