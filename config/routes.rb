f2bfdf424d6c4abeed16eee70016b63ace7f21ea
Maxime Zarka <maxime.zarka@gmail.com>
2018-11-26 18:02:24 +0100

    updated routes for booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
