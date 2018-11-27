110452d9e2b930bc9dbc439947f50d0cf4d9034b
Maxime Zarka <maxime.zarka@gmail.com>
2018-11-27 16:02:15 +0100

    modified schema

--- db/migrate/20181126161511_add_validated_to_bookings.rb
+++ db/migrate/20181126161511_add_validated_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddValidatedToBookings < ActiveRecord::Migration[5.2]
   def change
-    add_column :bookings, :validated, :boolean
+    add_column :bookings, :validated, :boolean, default: false
   end
 end
