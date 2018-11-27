ce5d08b6c551abfad4d987e716360c4bea881836
ovdb2001 <olivier.vdbroek@gmail.com>
2018-11-27 12:22:34 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
     collection do
       get 'ideas/search/:q', to: 'ideas#search', as: 'search'
     end
-    # member do
-    # end
     resources :bookings, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
