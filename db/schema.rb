bffe380f022c5c6221838460c37916a4195720ee
Maxime Zarka <maxime.zarka@gmail.com>
2018-11-27 17:20:23 +0100

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_153748) do
+ActiveRecord::Schema.define(version: 2018_11_27_160542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_153748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "request_message"
-    t.boolean "validated"
+    t.boolean "validated", default: false
     t.index ["idea_id"], name: "index_bookings_on_idea_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_153748) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.integer "upvotes", default: 0
+    t.float "avratings", default: 0.0
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
