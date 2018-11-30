3db27ffe47f2ac2d31f0aee0280b83f0167aa15a
xavier drieux <xavier.drieux@gmail.com>
2018-11-30 14:46:24 +0100

    sponsored links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_131247) do
+ActiveRecord::Schema.define(version: 2018_11_30_133645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_131247) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.boolean "is_ad", default: false
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
